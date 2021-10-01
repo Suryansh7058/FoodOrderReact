c1ff687e04a487dbf78753af09347528df85f41d
Suryansh <suryanshsingh7058@outlook.com>
2021-10-01 14:26:55 +0530

    Starting Project with GlobalStyles

---
+++ src/global/GlobalStyle.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    
+    * {
+    box-sizing: border-box;
+    }
+
+    html {
+        font-family: 'Noto Sans JP', sans-serif;
+    }
+
+    body {
+        margin: 0;
+    }
+
+`;
+
+export default GlobalStyle;
